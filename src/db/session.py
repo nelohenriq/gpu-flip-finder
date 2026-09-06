52a1f668c53a606106817c3924c0dc436abab4a3
nelohenriq <herminiohenriques@gmail.com>
2026-09-06 10:39:02 +0100

    fix: add DeclarativeBase to db session

--- src/db/session.py
+++ src/db/session.py
@@ -1,9 +1,14 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+
 from src.config.settings import settings
 
 engine = create_engine(settings.db_url, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+class Base(DeclarativeBase):
+    pass
 
 
 def init_db() -> None:
